c9bd9068d064a23772694055dc0cdcf97ae407ed
André Martinez <andremartinez92@gmail.com>
2024-01-22 01:01:57 +0000

    Change robots.txt to improve lighthouse score

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ const nextTranslate = require('next-translate-plugin');
 
 module.exports = nextTranslate({
   reactStrictMode: true,
+  // Improve first load speed
+  // https://github.com/vercel/next.js/discussions/13646#discussioncomment-7539607
   webpack: (config, { isServer }) => { if (!isServer) { config.optimization.splitChunks.maxSize = 30000; } return config; }
 });
