33523c8c28ac34438170e56ac85cc4b479e220d7
André Martinez <andremartinez92@gmail.com>
2024-02-26 17:43:18 +0100

    Small adjustments to netlify and sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,10 +2,12 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://d8eda799ac9cb866c0c9c5ffc7b0f2e6@o4505467578613760.ingest.sentry.io/4506162594316288",
+  enabled: process.env.NODE_ENV === 'production',
+
+  dsn: 'https://d8eda799ac9cb866c0c9c5ffc7b0f2e6@o4505467578613760.ingest.sentry.io/4506162594316288',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
